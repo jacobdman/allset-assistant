ecf61af8756320f51388ac7316c3423a2d25339a
Jacob Dayton <jacobdayton@Jacobs-MacBook-Air.local>
2023-07-01 16:04:29 -0600

    Updating some animation timing

--- src/components/Allset/PopupOption.js
+++ src/components/Allset/PopupOption.js
@@ -3,8 +3,12 @@ import "./allset.css";
 import CheckIcon from "@mui/icons-material/Check";
 import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
 import Typewriter from "typewriter-effect";
+import Grow from "@mui/material/Grow";
+import LinearProgress from "@mui/material/LinearProgress";
 
 const PopupOption = ({ close, optionTitle, optionText, actionText }) => {
+    // const [showCursor, setShowCursor] = useState(true);
+    const [loadingResponse, setLoadingResponse] = useState(true);
   const [responseGenerationCompleted, setResponseGenerationCompleted] =
     useState(false);
 
@@ -16,35 +20,43 @@ const PopupOption = ({ close, optionTitle, optionText, actionText }) => {
         <div className="optionTitle">{optionTitle}</div>
       </div>
 
+      <Grow in={loadingResponse} timeout={500} transitionDuration={2000}>
+        <div className="progress">
+          <LinearProgress />
+        </div>
+      </Grow>
+
       <Typewriter
         options={{
-          autoStart: true,
+          delay: 30,
+          cursor: "",
         }}
         onInit={typewriter => {
           typewriter
-            .pauseFor(2000)
+            .pauseFor(3000)
+            .callFunction(() => setLoadingResponse(false))
             .typeString(`<p>${optionText}</p>`)
-            .pauseFor(1000)
+            .pauseFor(500)
             .typeString(`<h3>${actionText}</h3>`)
             .callFunction(() => setResponseGenerationCompleted(true))
+            // .callFunction(() => setShowCursor(false))
             .start();
         }}
       />
-      {/* <p>{optionText}</p> */}
-      {/* <h3>{actionText}</h3> */}
-      {responseGenerationCompleted && (
-        <div className="actionButtons">
+      <div className="actionButtons">
+        <Grow in={responseGenerationCompleted} timeout={2000}>
           <button>
             <CheckIcon />
             Yes
           </button>
-          <span className="spacer"></span>
+        </Grow>
+        <Grow in={responseGenerationCompleted} timeout={2500}>
           <button>
             <QuestionMarkIcon />
             No
           </button>
-        </div>
-      )}
+        </Grow>
+      </div>
     </div>
   );
 };
